c69e62dc57bcdf36efeacfe4de5bf13ebc981252
sushilnamberdar <sushilnamberdar000@gmail.com>
2024-12-06 19:02:00 +0530

    login apge connect with the backend

--- src/Components/pages/LoginPage.js
+++ src/Components/pages/LoginPage.js
@@ -1,11 +1,76 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { BaseUrl } from '../../Utils/BaseUrl';
+import { toast } from 'react-toastify';
 
 const LoginPage = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(`${BaseUrl}/login`,formData);
+      console.log("response from the server ",response);
+      toast.success()
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div>
-      
+    <div className="space-y-6 bg-gray-50 mt-20 ml-auto mr-auto p-6 rounded-lg shadow-lg max-w-sm mx-auto">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-semibold text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            name="email"
+            type="email"
+            required
+            onChange={handleChange}
+            value={formData.email}
+            className="w-full mt-2 p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-semibold text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            required
+            value={formData.password}
+            onChange={handleChange}
+            className="w-full mt-2 p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
+            placeholder="Enter your password"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full mt-10  bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition duration-300 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+        >
+          Submit
+        </button>
+      </form>
     </div>
-  )
+  );
+
 }
 
 export default LoginPage
